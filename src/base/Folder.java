f6b941cf040ac1f578b1951dc7944da9a2a57760
Alan Li <a806400320@gmail.com>
2022-03-10 17:46:04 +0800

    lab3

--- src/base/Folder.java
+++ src/base/Folder.java
@@ -32,97 +32,97 @@ public class Folder implements Comparable<Folder> {
     }
 
     // public List<Note> searchNotes2(String keywords) {
-    //
-    // List<Note> noteReturn = new ArrayList<Note>(); // for the return result
-    // keywords = keywords.toLowerCase();
-    // String[] orFilter = keywords.split(" ");
-    // List<String> andArr = new ArrayList<String>(); // and list
-    // List<String> orArr = new ArrayList<String>(); // or list
-    //
-    // // add to or lists
-    // int i = 0;
-    // while (i < orFilter.length - 1) {
-    // if (orFilter[i + 1].equals("or")) {
-    // orArr.add(orFilter[i]);
-    // orArr.add(orFilter[i + 2]);
-    // i = i + 3;
-    // } else {
-    // andArr.add(orFilter[i]);
-    // i++;
-    // }
-    // }
-    // //// add to and lists
-    // // i = 0;
-    // // while (i < orFilter.length - 1) {
-    // // if (!(orFilter[i + 1].equals("or")) && !(orFilter[i].equals("or"))) {
-    // // andArr.add(orFilter[i]);
-    // // andArr.add(orFilter[i + 1]);
-    //
-    // // } else {
-    //
-    // // }
-    // // i++;
-    //
-    // // }
-    //
-    // System.out.println("And: ");
-    // for (String and : andArr) {
-    // System.out.println(and);
-    // }
-    //
-    // System.out.println("Or: ");
-    // for (String or : orArr) {
-    // System.out.println(or);
-    // }
-    //
-    // for (Note loopNote : notes) {
-    // // ImageNote
-    // if (loopNote instanceof ImageNote) {
-    // boolean andState = false;
-    // for (String and : andArr) {
-    // if (!(((ImageNote) loopNote).getTitle().toLowerCase().contains(and))) {
-    // andState = true;
-    // break;
-    // }
-    // }
-    // if (andState == false) {
-    // for (String or : orArr) {
-    // if (((ImageNote) loopNote).getTitle().toLowerCase().contains(or)) {
-    //
-    // noteReturn.add(loopNote);
-    // break;
-    //
-    // }
-    // }
-    // }
-    //
-    // } else if (loopNote instanceof TextNote) { // TextNote
-    // boolean andState = false;
-    // for (String and : andArr) {
-    // if (!((TextNote) loopNote).getContent().toLowerCase().contains(and)
-    // || !((TextNote) loopNote).getTitle().toLowerCase().contains(and)) {
-    // andState = true;
-    // break;
-    // }
-    // }
-    // if (andState == false) {
-    // for (String or : orArr) {
-    // if (((TextNote) loopNote).getContent().toLowerCase().contains(or)
-    // || ((TextNote) loopNote).getTitle().toLowerCase().contains(or)) {
-    // System.out.println("now: " + or +"\t" + orArr.get(orArr.size()-1));
-    // noteReturn.add(loopNote);
-    // break;
-    //
-    // }
-    // }
-    //
-    // }
-    // }
-    //
-    // }
-    //
-    // return noteReturn;
-    //
+
+    //     List<Note> noteReturn = new ArrayList<Note>(); // for the return result
+    //     keywords = keywords.toLowerCase();
+    //     String[] orFilter = keywords.split(" ");
+    //     List<String> andArr = new ArrayList<String>(); // and list
+    //     List<String> orArr = new ArrayList<String>(); // or list
+
+    //     // add to or lists
+    //     int i = 0;
+    //     while (i < orFilter.length - 1) {
+    //         if (orFilter[i + 1].equals("or")) {
+    //             orArr.add(orFilter[i]);
+    //             orArr.add(orFilter[i + 2]);
+    //             i = i + 3;
+    //         } else {
+    //             andArr.add(orFilter[i]);
+    //             i++;
+    //         }
+    //     }
+    //     //// add to and lists
+    //     // i = 0;
+    //     // while (i < orFilter.length - 1) {
+    //     // if (!(orFilter[i + 1].equals("or")) && !(orFilter[i].equals("or"))) {
+    //     // andArr.add(orFilter[i]);
+    //     // andArr.add(orFilter[i + 1]);
+
+    //     // } else {
+
+    //     // }
+    //     // i++;
+
+    //     // }
+
+    //     System.out.println("And: ");
+    //     for (String and : andArr) {
+    //         System.out.println(and);
+    //     }
+
+    //     System.out.println("Or: ");
+    //     for (String or : orArr) {
+    //         System.out.println(or);
+    //     }
+
+    //     for (Note loopNote : notes) {
+    //         // ImageNote
+    //         if (loopNote instanceof ImageNote) {
+    //             boolean andState = false;
+    //             for (String and : andArr) {
+    //                 if (!(((ImageNote) loopNote).getTitle().toLowerCase().contains(and))) {
+    //                     andState = true;
+    //                     break;
+    //                 }
+    //             }
+    //             if (andState == false) {
+    //                 for (String or : orArr) {
+    //                     if (((ImageNote) loopNote).getTitle().toLowerCase().contains(or)) {
+
+    //                         noteReturn.add(loopNote);
+    //                         break;
+
+    //                     }
+    //                 }
+    //             }
+
+    //         } else if (loopNote instanceof TextNote) { // TextNote
+    //             boolean andState = false;
+    //             for (String and : andArr) {
+    //                 if (!((TextNote) loopNote).getContent().toLowerCase().contains(and)
+    //                         || !((TextNote) loopNote).getTitle().toLowerCase().contains(and)) {
+    //                     andState = true;
+    //                     break;
+    //                 }
+    //             }
+    //             if (andState == false) {
+    //                 for (String or : orArr) {
+    //                     if (((TextNote) loopNote).getContent().toLowerCase().contains(or)
+    //                             || ((TextNote) loopNote).getTitle().toLowerCase().contains(or)) {
+    //                         System.out.println("now: " + or + "\t" + orArr.get(orArr.size() - 1));
+    //                         noteReturn.add(loopNote);
+    //                         break;
+
+    //                     }
+    //                 }
+
+    //             }
+    //         }
+
+    //     }
+
+    //     return noteReturn;
+
     // }
 
     public List<Note> searchNotes(String keywords) {
@@ -154,10 +154,10 @@ public class Folder implements Comparable<Folder> {
                                 || temp.getTitle().toLowerCase().contains(word2)
                                 || ((TextNote) temp).getContent().toLowerCase().contains(word1)
                                 || ((TextNote) temp).getContent().toLowerCase().contains(word2)) {
-//                            for (String t : wordlist) {
-//                                System.out.println(t);
-//                            }
-                            //System.out.println(word + "////" + wordlist.get(wordlist.size() - 1));
+                            // for (String t : wordlist) {
+                            // System.out.println(t);
+                            // }
+                            // System.out.println(word + "////" + wordlist.get(wordlist.size() - 1));
                             if (word.compareTo(wordlist.get(wordlist.size() - 1)) == 0) {
                                 // System.out.println(word + "////" + wordlist.get(wordlist.size() - 1));
                                 returnNote.add(temp);
